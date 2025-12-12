4865602fd387f088e508d145b3bbb1761acef447
Gaolaboratory <yugao@uic.edu>
2025-12-12 00:36:08 -0600

    Build: Commit Wasm pkg artifacts

---
+++ frontend/pkg/mass_spec_wasm.d.ts
@@ -0,0 +1,45 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function calculate_ions(sequence: string, charge: number): any;
+
+export function match_ions(peaks_val: any, theoretical_val: any, tolerance: number, tol_unit: string): any;
+
+export function parse_spectrum(xml_str: string): any;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly calculate_ions: (a: number, b: number, c: number) => any;
+  readonly parse_spectrum: (a: number, b: number) => any;
+  readonly match_ions: (a: any, b: any, c: number, d: number, e: number) => any;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_externrefs: WebAssembly.Table;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
